67ec171969cab63351744b4a32b41c5de4d73b7a
akrasnaya <an.krasnaya@gmail.com>
2022-12-14 00:20:20 +0300

    native dict

---
+++ task8.py
@@ -0,0 +1,27 @@
+class NativeDist:
+
+    # КОНСТРУКТОР
+    def __init__(self):
+        self.queue = []
+    # post-condition - создана пустая очередь
+
+    # КОМАНДЫ
+
+    def enqueue(self, value):
+    # post-condition - в хвост добавлен новый элемент со значением value
+
+    def dequeue(self):
+    # pre-condition - очередь не пустая
+    # post-condition - удален первый элемент очереди
+
+    # ЗАПРОСЫ
+
+    def size(self): # текущий размер очереди
+
+    def get_head(self): # получение первого элемента очереди
+    # pre-condition - очередь не пустая
+
+    # ЗАПРОСЫ СТАТУСОВ
+
+    def get_queue_status(self): # успешно - элемент добавлен
+    def get_deque_status(self): # успешно - элемент удален
